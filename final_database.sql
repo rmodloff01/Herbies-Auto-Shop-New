4036577459609d79c41d1d63982b76839ef6b642
kschick02 <kschick02@aurora.edu>
2017-10-26 20:59:41 -0500

    Improved database structure

---
+++ final_database.sql
@@ -0,0 +1,138 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema autoshop_development
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `autoshop_development` ;
+
+-- -----------------------------------------------------
+-- Schema autoshop_development
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `autoshop_development` DEFAULT CHARACTER SET utf8 ;
+
+USE `autoshop_development` ;
+
+-- -----------------------------------------------------
+-- Table `autoshop_development`.`customers`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `autoshop_development`.`customers` ;
+
+CREATE TABLE IF NOT EXISTS `autoshop_development`.`customers` (
+  `cust_id` INT NOT NULL,
+  `cust_fname` VARCHAR(15) NOT NULL,
+  `cust_lname` VARCHAR(15) NOT NULL,
+  `cust_phone` VARCHAR(13) NOT NULL,
+  `cust_addr` VARCHAR(30) NOT NULL,
+  `cust_date` DATETIME(0) NOT NULL,
+  PRIMARY KEY (`cust_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `autoshop_development`.`cars`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `autoshop_development`.`cars` ;
+
+CREATE TABLE IF NOT EXISTS `autoshop_development`.`cars` (
+  `car_model` VARCHAR(10) NOT NULL,
+  `car_make` VARCHAR(20) NOT NULL,
+  `car_year` VARCHAR(4) NOT NULL,
+  `car_vin` VARCHAR(17) NOT NULL,
+  `cust_id` INT NOT NULL,
+  `license_plate` VARCHAR(9) NOT NULL,
+  PRIMARY KEY (`car_vin`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `autoshop_development`.`employees`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `autoshop_development`.`employees` ;
+
+CREATE TABLE IF NOT EXISTS `autoshop_development`.`employees` (
+  `emp_fname` VARCHAR(20) NOT NULL,
+  `emp_lname` VARCHAR(20) NOT NULL,
+  `emp_id` VARCHAR(6) NOT NULL,
+  `emp_wage` DECIMAL(4,2) NOT NULL,
+  PRIMARY KEY (`emp_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `autoshop_development`.`invoices`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `autoshop_development`.`invoices` ;
+
+CREATE TABLE IF NOT EXISTS `autoshop_development`.`invoices` (
+  `cost_of_labor` DECIMAL(6,2) NULL DEFAULT NULL,
+  `car_vin` VARCHAR(17) NOT NULL,
+  `cust_id` INT NOT NULL,
+  `inv_id` INT NOT NULL,
+  `emp_id` VARCHAR(6) NOT NULL,
+  `inv_desc` VARCHAR(255) NULL DEFAULT NULL,
+  `prob_desc` VARCHAR(255) NOT NULL,
+  `eng_code` VARCHAR(45) NULL DEFAULT NULL,
+  `inv_date` DATE NOT NULL,
+  `tot_cost` DECIMAL(6,2) NULL DEFAULT NULL,
+  PRIMARY KEY (`inv_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `autoshop_development`.`suppliers`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `autoshop_development`.`suppliers` ;
+
+CREATE TABLE IF NOT EXISTS `autoshop_development`.`suppliers` (
+  `supp_id` INT NOT NULL,
+  `supp_name` VARCHAR(45) NOT NULL,
+  `supp_address` VARCHAR(55) NOT NULL,
+  PRIMARY KEY (`supp_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `autoshop_development`.`parts`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `autoshop_development`.`parts` ;
+
+CREATE TABLE IF NOT EXISTS `autoshop_development`.`parts` (
+  `part_id` INT NOT NULL,
+  `part_name` VARCHAR(20) NOT NULL,
+  `part_cost` DECIMAL(5,2) NOT NULL,
+  `part_num_in_inventory` INT NULL DEFAULT NULL,
+  `supp_id` INT NOT NULL,
+  PRIMARY KEY (`part_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `autoshop_development`.`included_parts`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `autoshop_development`.`included_parts` ;
+
+CREATE TABLE IF NOT EXISTS `autoshop_development`.`included_parts` (
+  `part_id` INT NOT NULL,
+  `inv_id` INT NOT NULL,
+  PRIMARY KEY (`part_id`, `inv_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
