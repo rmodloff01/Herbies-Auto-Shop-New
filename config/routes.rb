d29e7be981722d6324b96411be9a23f12cff966f
rmodloff01 <rmodloff01@aurora.edu>
2017-11-04 14:57:10 -0500

    Adding Devise Authentication
    Be careful, I hope this doesn’t break you apps…
    Remember to run rake db:migrate to add user stuff to you db

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  devise_for :users
   resources :suppliers
   resources :parts_includeds
   resources :issues
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: 'home#index'
+
+  resources :included_parts
+  resources :suppliers
   resources :invoices
   resources :parts
   resources :employees
